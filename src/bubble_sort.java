171dcdd94b9d782e3d2bbcb120d925837956712f
akshitkhandelwal2812 <akshit20012@gmail.com>
2021-02-10 17:52:16 +0530

    bubble sort

---
+++ src/bubble_sort.java
@@ -0,0 +1,39 @@
+public class bubble_sort {
+
+	
+		 void bsort(int  num[])
+		    {
+		        int n = num.length;
+		        for (int i = 0; i < n-1; i++)
+		            for (int j = 0; j < n-i-1; j++)
+		                if (num[j] > num[j+1])
+		                {
+		                    int temp = num[j];
+		                    num[j] = num[j+1];
+		                    num[j+1] = temp;
+		                }
+		    }
+		    
+		    void display(int num[])
+		    {
+		        int n = num.length;
+		        for (int i=0; i<n; ++i)
+		            System.out.print(num[i] + " ");
+		        System.out.println();
+		    }
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			bubble_sort bs = new bubble_sort();
+	        int num[] = {64, 34, 25, 12, 22, 11, 90};
+	        bs.bsort(num);
+	        System.out.println("Sorted array");
+	        bs.display(num);
+
+		}
+
+
+	}
+
+
